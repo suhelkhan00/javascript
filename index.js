8c611b60bafdd7a54d0245f9ce62f7e11957a1bb
suhelkhan00 <suhelkhk@gmail.com>
2024-09-14 20:21:57 +0530

    js1

--- index.js
+++ index.js
@@ -1,32 +1,103 @@
+// ********variables*********
+// let finame ="suhel"
+// console.log(finame)
+// const lname='khan'
+// console.log(lname)
+// const fname=`suhel
+// khan`
+// console.log(fname)
 
-const prompt = require('prompt-sync')()
-const num1 = parseFloat(prompt('Enter num1: '));
-const operator = prompt('Enter operator (+, -, *, /): ');
-const num2 = parseFloat(prompt('Enter num2: '));
+// *********function*****************
+// function funcname(){
+//     console.log('good boy')
+// }
+// funcname()
 
-let result;
+// function add(a,b,c,d){
+//     console.log(a+b+c+d)
+    
+// }
+// add(2,9,4,8)
 
-switch (operator) {
+// function mul(a,b,c,d){
+//     console.log(a*b*c*d)
+    
+// }
+// mul(2,9,4,8)
+
+// *********arrow function*****************
+// const func=()=>{
+//     console.log("Arrow Function")
+// }
+// func()
+
+// const func=(a,b,c)=>{
+//     console.log(a-b-c)
+// }
+// func(5,2,9)
+
+// const func=(a,b,c)=>{
+//     console.log(a/b/c)
+// }
+// func(5,2,9)
+
+// *************** to check typeof length trim touppercase tolowercase ***************
+// let str='the quick brown fox jumps over the lazy dog'
+// console.log(str)
+// console.log(typeof(str))
+
+// let index=str.indexOf()
+// console.log(index)
+
+// let upper=str.toUpperCase()
+// console.log(upper)
+
+// let low=str.toLowerCase()
+// console.log(low)
+
+// let str='  the quick brown fox jumps over the lazy dog         '
+// console.log(str)
+
+// let trim =str.trim()
+// console.log(trim)
+
+// let trim =str.trimStart()
+// console.log(trim)
+
+// let trim =str.trimEnd()
+// console.log(trim)
 
 
-case '+':
-    result = num1 + num2;
-    break;
-case '-':
-    result = num1 - num2;
-    break;
-case '*':
-    result = num1 * num2;
-    break;
-case '/':
-    if (num2 !== 0) {
-        result = num1 / num2;
-    } else {
-        result = 'Error: Division by zero!';
-    }
-    break;
-default:
-    result = 'Error: Invalid operator!';
+// **********to value from user *********
+const prompt=require('prompt-sync')()
+// let age=prompt('what is your age:')
+
+   
+//     if (age<=17) {
+//         console.log("minor") 
+//     } else {
+//         if(age<=35) {
+//              {
+//                 console.log("adult") 
+//             }
+//         }
+//         else{
+            
+//                 console.log("old") 
+            
+//         }
+//     }
+
+// let tri=prompt('print?:')
+// for (let i=0; i<=5; i++) {
+    
+//     console.log(tri.repeat(i));
+    
+// }
+
+let tri=prompt('print?:')
+for (let i=0; i<=5; i++) {
+    let s=' '
+    console.log(s.repeat(s)+tri.repeat(i-s));
+    
 }
-
-console.log(`Result: ${result}`);
